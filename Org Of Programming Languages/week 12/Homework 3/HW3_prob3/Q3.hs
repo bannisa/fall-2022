e697927b2267a5bbf47309e9ef2720754928047b
Ari Bannish <abannish9363@wwestfield.ma.edu>
2022-12-13 20:14:35 -0500

    12-13-2022

--- Org Of Programming Languages/week 12/Homework 3/HW3_prob3/Q3.hs
+++ Org Of Programming Languages/week 12/Homework 3/HW3_prob3/Q3.hs
@@ -9,21 +9,27 @@ type Deck = [Card]
 
 startingDeck::Deck
 startingDeck = [(a,"Spades") | a <-[1..13]] ++ [(a,"Clubs") | a <-[1..13]] ++ [(a,"Hearts") | a <-[1..13]] ++ [(a,"Diamonds") | a <-[1..13]]
+                
+                
+resetDeck:: IO Deck
+resetDeck = shuffle startingDeck
 
-removeCard::Deck -> Card -> Deck
-removeCard deck toRemove = delete toRemove deck
-
-shuffle::Deck -> Deck -> Deck
-shuffle shuffledDeck deck = deck
-
-resetDeck::Deck
-resetDeck =  shuffle startingDeck
-
-dealCard::Deck -> Int -> Card
-dealCard deck selectedCard = deck !! selectedCard
+dealCard:: IO Deck -> Int -> Card
+dealCard deck index = deck !! index
 
 
-gameLoop:: Deck -> IO ()
+removeCard::IO Deck -> Card -> Deck
+removeCard oldDeck cardtoDelete
+                                | length oldDeck == 1 = [curCard]
+                                | length oldDeck == 0 = [cardtoDelete]
+                                | curCard == cardtoDelete = oldDeckRemander
+                                | otherwise = [curCard] ++ (removeCard oldDeckRemander cardtoDelete)
+                                where
+                                    curCard = head oldDeck
+                                    oldDeckRemander = tail oldDeck
+
+
+gameLoop::IO Deck -> IO ()
 gameLoop deck = do
                  putStrLn "Select from options below"
                  putStrLn "1 Reset the deck."
@@ -42,13 +48,13 @@ gameLoop deck = do
                      putStrLn "Deck has been rest and shuffled"
 
                      gameLoop newDeck
-                 else if (userInput == 2) then do
+                 else if (userInt == 2) then do
                      let newDeck = shuffle deck
                      
                      putStrLn "Deck has been shuffled"
 
                      gameLoop newDeck
-                 else if (userInput == 3) then do
+                 else if (userInt == 3) then do
                      let min = 0
                      let max = length deck
                      
@@ -58,13 +64,13 @@ gameLoop deck = do
 
                      let newDeck = removeCard deck card
                      
-                     let cardStr = (show (fst card)) ++ ":" ++ (show (snd card))
+                     let cardStr = (show (fst card)) ++ ":" ++ (show (lst card))
                      
                      putStrLn "The removed card is:" ++ cardStr
 
                      gameLoop newDeck
                      
-                 else if (userInput == 4) then do
+                 else if (userInt == 4) then do
                      
                      putStrLn "Enter a number between 1 and 13"
                      userInput1 <- getLine
@@ -86,11 +92,11 @@ gameLoop deck = do
                      if(card == deltCard) then do
                          putStrLn "You got it correct!!!"
                          putStrLn cardStr
-                     else
+                     else do
                          putStrLn "You got it all wrong :("
                          gameLoop newDeck
                          
-                 else if (userInput == 5) then do
+                 else if (userInt == 5) then do
                      putStrLn "Thanks for playing, have a nice day!"
                  else
                      do
@@ -105,6 +111,6 @@ gameLoop deck = do
 main::IO ()
 main = do
     
-    gameLoop (shuffle startingDeck)
+    gameLoop (shuffle startingDeck (length startingDeck))
 
 
